4ed782afff1f1c65deed3cbfd466c1c99be6758d
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2024-06-11 11:15:52 +0530

    url

--- src/components/Upload/Uploader.jsx
+++ src/components/Upload/Uploader.jsx
@@ -22,7 +22,7 @@ const Uploader = ({ setUploader }) => {
     useEffect(()=>{
 
         const send = async()=>{
-           await axios.post(`http://localhost:8000/api/videos/addVid`, {title, desc, tags,videoUrl, imgUrl}).then( (res)=>{
+           await axios.post(`https://dinki-tube-server.vercel.app/api/videos/addVid`, {title, desc, tags,videoUrl, imgUrl}).then( (res)=>{
                 console.log(res.data);
                 navigate(`/video/${res.data._id}`)
            });
